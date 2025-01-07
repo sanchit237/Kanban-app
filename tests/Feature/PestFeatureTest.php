0828c31cf5cc345270e869c602865a90ffd18d56
sanchit237 <sanchitnemlekar8@gmail.com>
2025-01-07 10:19:51 +0530

    [TEST] Add test for to do api listing

--- tests/Feature/PestFeatureTest.php
+++ tests/Feature/PestFeatureTest.php
@@ -1,7 +1,15 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
+
+test('to do api listing', function () {
+
+    $user = User::first();
+
+    Sanctum::actingAs($user);
+
+    $response = $this->postJson('/api/todos');
 
     $response->assertStatus(200);
 });
